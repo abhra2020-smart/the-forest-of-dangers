510d05fee7f175d40d2c5fddf6b42f1a1cd508ed
abhra2020-smart <68565872+abhra2020-smart@users.noreply.github.com>
2020-10-31 15:37:59 +0000

    Update main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let Tree: Sprite = null
 scene.setBackgroundColor(9)
 let Abby = sprites.create(sprites.castle.heroWalkFront1, SpriteKind.Player)
-Abby.setPosition(123, 49)
+Abby.setPosition(75, 55)
 controller.moveSprite(Abby)
 for (let index = 0; index < randint(5, 160); index++) {
     Tree = sprites.create(sprites.castle.treeSmallPine, SpriteKind.Projectile)
